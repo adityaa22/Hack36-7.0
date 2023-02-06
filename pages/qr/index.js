aeff952582e0ac2a508c21642d3c4e93b2410907
DuP-491 <divyansh.20198117@mnnit.ac.in>
2023-02-06 22:59:13 +0530

    Added QR

---
+++ pages/qr/index.js
@@ -0,0 +1,9 @@
+import Head from "next/head"
+
+export default function Document() {
+  return (
+   <>
+   <Head> <meta http-equiv="refresh" content="3;url=https://www.youtube.com/watch?v=dQw4w9WgXcQ" /></Head>
+   </>
+  )
+}

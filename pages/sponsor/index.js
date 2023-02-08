00a91f2dc60b9d65a43c494b11a6daee68e5adac
DuP-491 <divyansh.20198117@mnnit.ac.in>
2023-02-09 02:16:32 +0530

    Added flex

--- pages/sponsor/index.js
+++ pages/sponsor/index.js
@@ -1,24 +1,47 @@
 import Head from "next/head";
 
-export default function sponsor(){
-    return (
-        <>
-        <h1>sponsor</h1>
-        <div className="container">
-            <h1>Giga Sponsor</h1>
-        <div className="flex">
-        <img src="./sponsor/Devfolio_Logo-Colored.png"></img>
-        <img src="./sponsor/Polygon_Logo-Colored.png"></img>
+export default function sponsor() {
+  return (
+    <>
+    <div className="container">
+      <h1 className="text-center">Sponsor</h1>
+    </div>
+      <div className="container">
+        <h1 className="text-center">Title Sponsor</h1>
+      </div>
+
+      <div className="container">
+        <h1 className="text-center">Terra Sponsor</h1>
+      </div>
+
+      <div className="container">
+        <h1 className="text-center">Giga Sponsor</h1>
+        <div className="flex justify-around">
+          <div><img src="./sponsor/Devfolio_Logo-Colored.png"></img></div>
+          <div><img src="./sponsor/Polygon_Logo-Colored.png"></img></div>     
         </div>
+      </div>
+
+      <div className="container">
+        <h1 className="text-center">Mega Sponsor</h1>
+        <div className="flex justify-center">
+          <div>
+          <img
+            className="w-full h-full"
+            src="./sponsor/Filecoin Coloured White Text-1.png"
+          ></img>
+          </div>
+          <div>
+          <img className="w-full h-full" src="./sponsor/Solana-Colored.png"></img>
+          </div>
+          <div>
+          <img
+            className="w-full h-full"
+            src="./sponsor/Replit-light-background.png"
+          ></img>
+          </div>
         </div>
-        <div className="container">
-        <h1>Mega Sponsor</h1>
-        <div className="flex">
-        <img src="./sponsor/Filecoin Coloured White Text-1.png"></img>
-        <img src="./sponsor/Solana-Colored.png"></img>
-        <img src="./sponsor/Replit-light-background.png"></img>
-        </div>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }

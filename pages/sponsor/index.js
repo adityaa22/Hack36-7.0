63b29ab7c4ffea76f7aeef329d07884d787688ba
DuP-491 <divyansh.20198117@mnnit.ac.in>
2023-02-09 01:25:20 +0530

    Added new sponsors

---
+++ pages/sponsor/index.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+
+export default function sponsor(){
+    return (
+        <>
+        <h1>sponsor</h1>
+        <div className="container">
+            <h1>Giga Sponsor</h1>
+        <div className="flex">
+        <img src="./sponsor/Devfolio_Logo-Colored.png"></img>
+        <img src="./sponsor/Polygon_Logo-Colored.png"></img>
+        </div>
+        </div>
+        <div className="container">
+        <h1>Mega Sponsor</h1>
+        <div className="flex">
+        <img src="./sponsor/Filecoin Coloured White Text-1.png"></img>
+        <img src="./sponsor/Solana-Colored.png"></img>
+        <img src="./sponsor/Replit-light-background.png"></img>
+        </div>
+        </div>
+        </>
+    )
+}

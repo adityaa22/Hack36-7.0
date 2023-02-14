a23993471bc9bda4d8f491c7fbe655379deffab9
lovedeep singh kamal <developer.lovedeep@gmail.com>
2023-02-14 23:34:44 +0530

    fixed group hover on prize card

--- components/Prizes.js
+++ components/Prizes.js
@@ -4,7 +4,7 @@ export default function Prizes() {
       <h1 className="mt-4 font-bold text-center uppercase text-7xl">Prizes</h1>
 
       <div className="flex items-center justify-center mt-16">
-        <div className="-mr-4 overflow-hidden transition-all duration-300 shadow-lg card w-80 h-96 rounded-2xl hover:z-20 hover:scale-105">
+        <div className="-mr-4 overflow-hidden transition-all duration-300 shadow-lg group card w-80 h-96 rounded-2xl hover:z-20 hover:scale-105">
           <div className="relative h-40">
             <div
               className="absolute inset-0"
@@ -16,7 +16,7 @@ export default function Prizes() {
             <div className="absolute inset-0  bg-[url('/Snitch.png')] grayscale bg-[length:60%] bg-center bg-no-repeat"></div>
           </div>
           {/* <Image src={Snitch} className="object-contain h-40" /> */}
-          <div className="px-8 text-gray-800 content bg-[#ffebb3] hover:bg-[#ffd86b]  h-full ">
+          <div className="px-8 text-gray-800 content bg-[#ffebb3] group-hover:bg-[#ffd86b]  h-full ">
             <h1 className="py-4 text-2xl font-semibold capitalize">
               Ace of Cups
             </h1>
@@ -27,7 +27,7 @@ export default function Prizes() {
             </p>
           </div>
         </div>
-        <div className="z-10 -mt-16 overflow-hidden transition-all duration-300 shadow-lg card w-80 h-96 rounded-2xl hover:scale-105">
+        <div className="z-10 -mt-16 overflow-hidden transition-all duration-300 shadow-lg group card w-80 h-96 rounded-2xl hover:scale-105">
           <div className="relative h-40">
             <div
               className="absolute inset-0"
@@ -39,7 +39,7 @@ export default function Prizes() {
             <div className="absolute inset-0  bg-[url('/Snitch.png')]  bg-[length:60%] bg-center bg-no-repeat"></div>
           </div>
           {/* <Image src={Snitch} className="object-contain h-40" /> */}
-          <div className="px-8 text-gray-800 content bg-[#ffebb3] hover:bg-[#ffd86b] h-full">
+          <div className="px-8 text-gray-800 content bg-[#ffebb3] group-hover:bg-[#ffd86b] h-full">
             <h1 className="py-4 text-2xl font-semibold capitalize">
               Ace of Cups
             </h1>
@@ -50,7 +50,7 @@ export default function Prizes() {
             </p>
           </div>
         </div>
-        <div className="-ml-4 overflow-hidden transition-all duration-300 shadow-lg card w-80 h-96 hover:z-20 rounded-2xl hover:scale-105">
+        <div className="-ml-4 overflow-hidden transition-all duration-300 shadow-lg group card w-80 h-96 hover:z-20 rounded-2xl hover:scale-105">
           <div className="relative h-40">
             <div
               className="absolute inset-0"
@@ -59,10 +59,10 @@ export default function Prizes() {
                 backdropFilter: "blur(2px)",
               }}
             ></div>
-            <div className="absolute inset-0  bg-[url('/Snitch.png')] sepia bg-[length:60%] bg-center bg-no-repeat"></div>
+            <div className="absolute inset-0  bg-[url('/Snitch.png')] sepia  bg-[length:60%] bg-center bg-no-repeat"></div>
           </div>
           {/* <Image src={Snitch} className="object-contain h-40" /> */}
-          <div className=" bg-[#ffebb3] hover:bg-[#ffd86b]  h-full">
+          <div className=" bg-[#ffebb3] group-hover:bg-[#ffd86b]  h-full">
             <div className="px-8 text-gray-800">
               <h1 className="py-4 text-2xl font-semibold capitalize">
                 Ace of Cups

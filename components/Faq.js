11cbde7b746382bf5d1b00b98c60d382a4da4c8b
lovedeep singh kamal <developer.lovedeep@gmail.com>
2023-02-17 01:07:36 +0530

    faq and show case

--- components/Faq.js
+++ components/Faq.js
@@ -16,10 +16,13 @@ export const Faq = () => {
               itemprop="name"
               className="p-4 bg-gray-600 rounded-md cursor-pointer"
             >
-              A question about something
+              Do I Need To Pay Any Money To Register ?
             </summary>
             <div class="bg-gray-600 my-4 rounded-md p-4">
-              <p>The answer about this has some points to make:</p>
+              <p>
+                No. You do not have to pay anything to anyone to register
+                yourself for Hack36
+              </p>
             </div>
           </details>
           <details className="w-4/5 mx-auto">
@@ -27,10 +30,13 @@ export const Faq = () => {
               itemprop="name"
               className="p-4 bg-gray-600 rounded-md cursor-pointer"
             >
-              A question about something
+              Who Can Apply?
             </summary>
             <div class="bg-gray-600 my-4 rounded-md p-4">
-              <p>The answer about this has some points to make:</p>
+              <p>
+                Anyone having a knack of solving real life problems can apply
+                for the hackathon.
+              </p>
             </div>
           </details>
           <details className="w-4/5 mx-auto">
@@ -38,10 +44,78 @@ export const Faq = () => {
               itemprop="name"
               className="p-4 bg-gray-600 rounded-md cursor-pointer"
             >
-              A question about something
+              What Is The Procedure Of Forming A Team?
             </summary>
             <div class="bg-gray-600 my-4 rounded-md p-4">
-              <p>The answer about this has some points to make:</p>
+              <p>
+                After registration, participants are required to form their team
+                by logging in
+              </p>
+            </div>
+          </details>
+          <details className="w-4/5 mx-auto">
+            <summary
+              itemprop="name"
+              className="p-4 bg-gray-600 rounded-md cursor-pointer"
+            >
+              What We Can Build?
+            </summary>
+            <div class="bg-gray-600 my-4 rounded-md p-4">
+              <p>
+                You can build anything you are passionate about like some web
+                app, mobile app or something using hardware.
+              </p>
+            </div>
+          </details>
+          <details className="w-4/5 mx-auto">
+            <summary
+              itemprop="name"
+              className="p-4 bg-gray-600 rounded-md cursor-pointer"
+            >
+              Will there still be goodies and swag?
+            </summary>
+            <div class="bg-gray-600 my-4 rounded-md p-4">
+              <p>Definitely yes!</p>
+            </div>
+          </details>
+          <details className="w-4/5 mx-auto">
+            <summary
+              itemprop="name"
+              className="p-4 bg-gray-600 rounded-md cursor-pointer"
+            >
+              How Will The Shortlisting Be Done?
+            </summary>
+            <div class="bg-gray-600 my-4 rounded-md p-4">
+              <p>
+                Participants will be shortlisted based on their GitHub profile
+                and resume.
+              </p>
+            </div>
+          </details>
+          <details className="w-4/5 mx-auto">
+            <summary
+              itemprop="name"
+              className="p-4 bg-gray-600 rounded-md cursor-pointer"
+            >
+              What Can Be The Size Of The Team?
+            </summary>
+            <div class="bg-gray-600 my-4 rounded-md p-4">
+              <p>A team can consist of 2 to 4 members.</p>
+            </div>
+          </details>
+          <details className="w-4/5 mx-auto">
+            <summary
+              itemprop="name"
+              className="p-4 bg-gray-600 rounded-md cursor-pointer"
+            >
+              What Is The Criteria For Selection Of A Team?
+            </summary>
+            <div class="bg-gray-600 my-4 rounded-md p-4">
+              <p>
+                If 75% members of a team have a decent profile, the team will
+                get shortlisted. In a rare case where a team is not shortlisted,
+                that team can contact us for the re-evaluation of their team.
+              </p>
             </div>
           </details>
         </div>
--- components/ShowCase.js
+++ components/ShowCase.js
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import React from "react";
-import LandingImage from "../public/landing.png";
-import LandingImage2 from "../public/magicroom.jpg";
+import LandingImage from "../public/landing/singer.jpeg";
+import LandingImage2 from "../public/landing/swags.jpeg";
+import LandingImage3 from "../public/landing/tableTennis.jpeg";
+import LandingImage4 from "../public/landing/redbull.jpeg";
+import LandingImage5 from "../public/landing/hack36T.jpeg";
 import Landing from "./Landing";
 
 export const ShowCase = () => {
   return (
     <section className="md:py-16">
       <div className="p-4 space-y-1 text-center">
-        <h1 className="text-sm font-medium uppercase ">
+        <h1 className="text-sm font-medium uppercase font-harry-font">
           its not just about coding
         </h1>
         <h1 className="text-3xl font-bold uppercase md:text-5xl">
@@ -19,10 +22,12 @@ export const ShowCase = () => {
         <div className="animate-horizontal-showcase grid shrink-0 grid-cols-2 grid-rows-2 gap-3 w-[30rem] md:w-[40rem]">
           <Image
             src={LandingImage}
+            alt="Error"
             className="object-cover w-full h-full col-span-1 row-span-2 rounded-lg shadow-lg"
           />
           <Image
-            src={LandingImage2}
+            src={LandingImage4}
+            alt="Error"
             className="object-cover w-full h-full row-span-1 rounded-lg shadow-lg"
           />
           <div className="flex items-center justify-center w-full h-full row-span-1 p-4 text-center rounded-lg shadow-lg bg-gradient-to-r from-gray-900 to-gray-800">
@@ -31,7 +36,7 @@ export const ShowCase = () => {
                 750+
               </h1>
               <p className="text-sm leading-5 uppercase">
-                teams participated in hack36 5.0
+                Teams participated in hack36 5.0
               </p>
             </div>
           </div>
@@ -39,7 +44,8 @@ export const ShowCase = () => {
 
         <div className="animate-horizontal-showcase grid shrink-0 grid-cols-2 grid-rows-2 gap-3 w-[30rem] md:w-[40rem]">
           <Image
-            src={LandingImage}
+            src={LandingImage5}
+            alt="error"
             className="object-cover w-full h-full col-span-1 row-span-2 rounded-lg shadow-lg"
           />
           <div className="flex items-center justify-center w-full h-full row-span-1 p-4 text-center rounded-lg shadow-lg bg-gradient-to-r from-gray-900 to-gray-800">
@@ -51,7 +57,8 @@ export const ShowCase = () => {
             </div>
           </div>
           <Image
-            src={LandingImage2}
+            src={LandingImage3}
+            alt="error"
             className="object-cover w-full h-full row-span-1 rounded-lg shadow-lg"
           />
         </div>

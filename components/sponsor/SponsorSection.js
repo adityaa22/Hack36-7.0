758b6804167c60002dc29a5854f73db933db32e3
DuP-491 <divyansh.20198117@mnnit.ac.in>
2023-02-17 14:46:04 +0530

    Added key

--- components/sponsor/SponsorSection.js
+++ components/sponsor/SponsorSection.js
@@ -6,8 +6,9 @@ export default function SponsorSection({ sectionName, data }) {
       <section>
         <h1 className="text-3xl font-bold text-center">{sectionName}</h1>
         <div className="flex flex-wrap items-center justify-center gap-4 p-4">
-          {data.map((sponsor) => (
+          {data.map((idx,sponsor) => (
             <SponsorCard
+              key={idx}
               name={sponsor.name}
               url={sponsor.url}
               imageLink={sponsor.imageLink}

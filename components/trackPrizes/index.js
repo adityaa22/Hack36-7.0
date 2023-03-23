157c44435fd581530d11b8e960e8e75903eabe16
lovedeep singh kamal <developer.lovedeep@gmail.com>
2023-03-23 22:53:47 +0530

    added show more button on track prize

--- components/trackPrizes/index.js
+++ components/trackPrizes/index.js
@@ -1,10 +1,17 @@
 import TrackPrize from "./TrackPrize";
 import trackPrizeData from "../../data/trackPrizeData";
+import { useState } from "react";
 const TrackPrizes = () => {
+  const [showMore, setShowMore] = useState(false);
+
   return (
     <section className="Track">
       <div className="2xl:container">
-        <div className="flex flex-wrap justify-center gap-4 mx-4 mt-12 gap-y-8">
+        <div
+          className={`flex flex-wrap justify-center gap-4 mx-4 mt-12 overflow-hidden transition-all gap-y-8 ${
+            showMore ? "h-auto" : "h-96"
+          } `}
+        >
           {trackPrizeData.map((prize, idx) => {
             return (
               <TrackPrize
@@ -17,6 +24,14 @@ const TrackPrizes = () => {
             );
           })}
         </div>
+        <p className="text-center">
+          <button
+            onClick={() => setShowMore(!showMore)}
+            className="inline-block px-4 py-2 mx-auto mt-4 text-gray-100 transition-all bg-gray-800 rounded-md cursor-pointer hover:bg-gray-900 capitatext-center"
+          >
+            {showMore ? "Show Less" : "Show More"}
+          </button>
+        </p>
       </div>
     </section>
   );

8e1704070ea27d89d7f0635e3131030fd367934f
Shashank Verma <shashank.verma2002@gmail.com>
2024-03-28 20:36:46 +0530

    Fix about content
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -7,14 +7,14 @@
     >
         <div class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed">
             <div
-                class="h-[18rem] w-full z-10 overflow-clip absolute bg-no-repeat bg-cover bottom-[-8rem] clouds"
+                class="h-[21rem] w-full z-10 overflow-clip absolute bg-no-repeat bg-cover bottom-[-9rem] clouds"
             />
             <div
                 class="mx-auto max-w-7xl px-6 py-12 sm:py-12 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-24"
             >
                 <div class="h-full pt-12 lg:pt-0 mx-0 max-w-2xl lg:flex-auto text-center">
-                    <h1 class="mt-24 text-6xl">About Us</h1>
-                    <p class="mt-6 text-2xl">
+                    <h1 class="mt-6 md:mt-24 text-6xl">About Us</h1>
+                    <p class="text-2xl">
                         Hack36 7.0 returns with excitement and anticipation, offering a hybrid
                         hackathon experience. Teams of 2-4 college students can brainstorm ideas and
                         showcase their programming skills. The 36-hour exhibition features talks,
